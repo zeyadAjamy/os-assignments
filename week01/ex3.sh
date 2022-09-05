51b741a5527a117de92302591fa598fd51368116
Zeyad Alagamy <wezza2003@gmail.com>
2022-09-05 22:47:24 +0300

    fixed ex3

--- week01/ex3.sh
+++ week01/ex3.sh
@@ -3,20 +3,26 @@ date_wait(){
 	sleep 3
 }
 
+# Remove the Root and Home folders if exist
+cd ~/week01/
+rm -r -f Root Home
+
 date_wait
 mkdir Root && cd Root
-date_wait
 
+date_wait
 touch root.txt
 cd / && ls -t -r >  ~/week01/Root/root.txt 
-date_wait
 
+date_wait
 cd ~/week01/
 mkdir Home && cd Home
-date_wait
 
+date_wait
 touch home.txt
 cd ~ && ls -t -r > ~/week01/Home/home.txt
+cat ~/week01/Root/root.txt && cat ~/week01/Home/home.txt
+ls ~/week01/Root && ls ~/week01/Home 
 
 exit
 
