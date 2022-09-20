3762dae4532b6858daa5a6dd5a0f4dfc7705a3d9
Zeyad Alagamy <wezza2003@gmail.com>
2022-09-20 23:38:48 +0300

    solved

--- week03/ex4.c
+++ week03/ex4.c
@@ -1,58 +1,78 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void *add(const void *a, const void *b)
-{
-    void *result = malloc(sizeof(double));
-    *(double *)result = *(double *)a + *(double *)b;
-    return result;
-}
-
-void *multiply(const void *a, const void *b)
-{
-    void *result = malloc(sizeof(double));
-    *(double *)result = *(double *)a * *(double *)b;
-    return result;
-}
-
-void *mean(const void *a, const void *b)
-{
-    void *result = malloc(sizeof(double));
-    *(double *)result = (*(double *)a + *(double *)b) / 2;
-    return result;
-}
-
-void *aggregate(void *base, size_t size, int n, void *initial_value, void *(*opr)(const void *, const void *))
+void *aggregate(void *base, size_t size, int n, void *initial_value,
+                void *(*opr)(const void *, const void *))
 {
     void *result = malloc(size);
     memcpy(result, initial_value, size);
     for (int i = 0; i < n; i++)
     {
-        void *temp = opr(result, base + i * size);
-        memcpy(result, temp, size);
-        free(temp);
+        void *item = malloc(size);
+        memcpy(item, base + i * size, size);
+        result = opr(result, item);
     }
     return result;
 }
 
+void *addInt(const void *a, const void *b){
+    int *result = malloc(sizeof(int));
+    *result = *(int *)a + *(int *)b;
+    return result;
+}
+
+void *addDouble(const void *a, const void *b){
+    double *result = malloc(sizeof(double));
+    *result = *(double *)a + *(double *)b;
+    return result;
+}
+
+void *multiplyInt(const void *a, const void *b){
+    int *result = malloc(sizeof(int));
+    *result = *(int *)a * *(int *)b;
+    return result;
+}
+
+void *multiplyDouble(const void *a, const void *b){
+    double *result = malloc(sizeof(double));
+    *result = *(double *)a * *(double *)b;
+    return result;
+}
+
+
+void *meanInt(const void *a, const void *b){
+    int *result = malloc(sizeof(int));
+    *result = (*(int *)a + *(int *)b) / 2;
+    return result;
+}
+
+void *meanDouble(const void *a, const void *b){
+    double *result = malloc(sizeof(double));
+    *result = (*(double *)a + *(double *)b) / 2;
+    return result;
+}
 
 int main()
 {
-    double initial_value = 0;
-    double arr[] = {1, 2, 3, 4, 5};
-    double *result = aggregate((void *)arr, sizeof(double), 5,
-                               (void *)&initial_value, mean);
-    printf("mean: %f %f %f %f %f = %f", arr[0], arr[1], arr[2], arr[3], arr[4], *result);
-    free(result);
-    printf("\n");
+    double a[] = {1, 2, 3, 4, 5};
+    int b[] = {1, 2, 3, 4, 5};
 
-    int arr2[] = {1, 2, 3, 4, 5};
-    int *result2 = aggregate((void *)arr2, sizeof(int), 5,
-                             (void *)&initial_value, multiply);
-    printf("Multiply: %d %d %d %d %d = %d", arr2[0], arr2[1], arr2[2], arr2[3], arr2[4], *result2);
-    free(result2);
-    printf("\n");
+    double intial_value = 0;
+    double *result = aggregate(a, sizeof(double), 5, &intial_value, addDouble);
+
+    printf("%f\n", *result);
+
+    int intial_value2 = 1;
+    int *result2 = aggregate(b, sizeof(int), 5, &intial_value2, multiplyInt);
+
+    printf("%d\n", *result2);
+
+    double intial_value3 = 0;
+    double *result3 = aggregate(a, sizeof(double), 5, &intial_value3, meanDouble);
+
+    printf("%f\n", *result3);
 
     return 0;
 }
