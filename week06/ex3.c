21e154ff3ae945793b276991898ccca5728224d3
zeyadAjamy <wezza2003@gmail.com>
2022-10-11 23:55:00 +0300

    Solved assignment 6

--- week06/ex3.c
+++ week06/ex3.c
@@ -1,129 +1,143 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
-    int n = 0, i = 0, j, k, temp;
-    int *at, *bt, *ct, *tat, *wt;
-    float avg_tat, avg_wt;
-    int quantum;
+#define MAX 100
 
-    // Get the numbe of processes
+typedef struct process {
+    int arrival_time;
+    int burst_time;
+    int remaining_time;
+    int waiting_time;
+    int turnaround_time;
+} process;
+
+void sort(process *p, int n) {
+    int i, j;
+    process temp;
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (p[i].arrival_time > p[j].arrival_time) {
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+}
+
+
+int main() {
+    int n, time = 0, total_waiting_time = 0, total_turnaround_time = 0;
+    float average_waiting_time, average_turnaround_time;
+    process p[MAX];
     printf("Enter the number of processes: ");
     scanf("%d", &n);
-
-    if (n < 1)
-    {
-        printf("At least one process is required \n");
-        return 1;
+    for (int i = 0; i < n; i++) {
+        printf("Enter the arrival time and burst time for process %d: ", i + 1);
+        scanf("%d %d", &p[i].arrival_time, &p[i].burst_time);
+        p[i].remaining_time = p[i].burst_time;
     }
+    sort(p, n);
 
-    at = (int *)malloc(n * sizeof(int));
-    bt = (int *)malloc(n * sizeof(int));
-    ct = (int *)malloc(n * sizeof(int));
-    tat = (int *)malloc(n * sizeof(int));
-    wt = (int *)malloc(n * sizeof(int));
-
-    // Get the arrival time and burst time of each process
-    for (i = 0; i < n; i++)
-    {
-        // Get the arrival time and burst time of each process
-        printf("Enter the arrival time of process %d: ", i + 1);
-        scanf("%d", &at[i]);
-        printf("Enter the burst time of process %d: ", i + 1);
-        scanf("%d", &bt[i]);
-    }
-
-    // sort the processes based on the arriving time
-    for (i = 0; i < n; i++)
-    {
-        for (j = i + 1; j < n; j++)
-        {
-            if (at[i] > at[j])
-            {
-                temp = at[i];
-                at[i] = at[j];
-                at[j] = temp;
-
-                temp = bt[i];
-                bt[i] = bt[j];
-                bt[j] = temp;
-            }
-        }
-    }
-
-    // Get the quantum and on enter, print the metrics
+    // Round Robin Scheduling considring the arrival time of the processes
+    int quantum;
     printf("Enter the quantum: ");
-    while (scanf("%d", &quantum) == 1)
-        break;
+    scanf("%d", &quantum);
 
-    // Round robin algorithm
-    int time = 1;
-    int *ready_queue = (int *)malloc(n * sizeof(int));
-    int *bt_copy = (int *)malloc(n * sizeof(int));
     int remaining = n;
-    // Round robin algorithm consdiering the arrival time of the processes
-    for(i = 0; i < n; i++)
-    {
-        bt_copy[i] = bt[i];
+    int done[n];
+    for (int i = 0; i < n; i++) {
+        done[i] = 0;
     }
-    int min_bt = 9999, min_bt_index = 0;
-    while (remaining > 0)
-    {
-        for (i = 0; i < n; i++)
-        {
-            if (at[i] <= time && bt_copy[i] < min_bt && bt_copy[i] > 0)
-            {
-                min_bt = bt_copy[i];
-                min_bt_index = i;
+    // create a queue
+    int queue[n];
+    int front = 0, rear = 0;
+    int index = 0;
+     // find min arrival time
+    int min = 0;
+    for (int j = 0; j < n; j++) {
+        if (p[j].arrival_time < p[min].arrival_time) {
+            min = j;
+        }
+    }
+    time = p[min].arrival_time;
+    // Handle quantum time equal to 1
+    if (quantum == 1) {
+        while (remaining > 0) {
+            for (int i = 0; i < n; i++) {
+                if (p[i].arrival_time <= time && done[i] == 0) {
+                    queue[rear] = i;
+                    rear++;
+                }
+            }
+            if (front == rear) {
+                time++;
+                continue;
+            }
+            index = queue[front];
+            front++;
+            p[index].remaining_time--;
+            time++;
+            if (p[index].remaining_time == 0) {
+                p[index].turnaround_time = time - p[index].arrival_time;
+                p[index].waiting_time = p[index].turnaround_time - p[index].burst_time;
+                total_waiting_time += p[index].waiting_time;
+                total_turnaround_time += p[index].turnaround_time;
+                done[index] = 1;
+                remaining--;
             }
         }
-        if (min_bt == 9999)
-        {
-            time++;
-            continue;
+    } else {
+        while (remaining > 0) {
+            for (int i = 0; i < n; i++) {
+                if (p[i].arrival_time <= time && done[i] == 0) {
+                    queue[rear] = i;
+                    rear++;
+                }
+            }
+            if (front == rear) {
+                time++;
+                continue;
+            }
+            index = queue[front];
+            front++;
+            if (p[index].remaining_time > quantum) {
+                p[index].remaining_time -= quantum;
+                time += quantum;
+            } else {
+                time += p[index].remaining_time;
+                p[index].remaining_time = 0;
+            }
+            if (p[index].remaining_time == 0) {
+                p[index].turnaround_time = time - p[index].arrival_time;
+                p[index].waiting_time = p[index].turnaround_time - p[index].burst_time;
+                total_waiting_time += p[index].waiting_time;
+                total_turnaround_time += p[index].turnaround_time;
+                done[index] = 1;
+                remaining--;
+            } else {
+                queue[rear] = index;
+                rear++;
+            }
         }
-        if (bt_copy[min_bt_index] <= quantum)
-        {
-            time += bt_copy[min_bt_index];
-            bt_copy[min_bt_index] = 0;
-            remaining--;
-            ct[min_bt_index] = time;
-        }
-        else
-        {
-            bt_copy[min_bt_index] -= quantum;
-            time += quantum;
-        }
-        min_bt = 9999;
     }
 
-    // Calculate the turn around time and waiting time
-    for (i = 0; i < n; i++)
-    {
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
+    for (int i = 0; i < n; i++) {
+        total_waiting_time += p[i].waiting_time;
+        total_turnaround_time += p[i].turnaround_time;
     }
 
-    // calculate the average turnaround time and waiting time
-    avg_tat = 0;
-    avg_wt = 0;
-    for (i = 0; i < n; i++)
-    {
-        avg_tat += tat[i];
-        avg_wt += wt[i];
+    average_waiting_time = (float) total_waiting_time / n;
+    average_turnaround_time = (float) total_turnaround_time / n;
+
+    printf("Process\tAT\tBT\tCT\tTT\tWT\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\t%d\t%d\t%d \n", i + 1, p[i].arrival_time, p[i].burst_time, p[i].turnaround_time + p[i].arrival_time, p[i].turnaround_time, p[i].waiting_time);
     }
 
-    avg_tat /= n;
-    avg_wt /= n;
-
-    printf("Process\tAT\tBT\tCT\tTAT\tWT\n");
-
-    for (i = 0; i < n; i++)
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
-
-    printf("Average Turnaround Time: %f\n", avg_tat);
-    printf("Average Waiting Time: %f\n", avg_wt);
-
+    printf("Average waiting time: %f\n", average_waiting_time);
+    printf("Average turnaround time: %f\n", average_turnaround_time);
     return 0;
 }
---
+++ week06/ex3.sh
@@ -0,0 +1,6 @@
+# compile
+gcc -o ex3 ex3.c
+# run
+./ex3
+# clean
+rm ex3
