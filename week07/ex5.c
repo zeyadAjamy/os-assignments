ae03cd7baae7c28ea7ee237d5a15cd106e260369
zeyadAjamy <wezza2003@gmail.com>
2022-10-25 23:55:00 +0300

    edited solution 5

--- week07/ex5.c
+++ week07/ex5.c
@@ -6,8 +6,6 @@ int main () {
     char foo[] = "Hello World";
     *s = (char *) foo;
     printf("s is %s\n", *s);
-    s[0] = *foo;
-
-    printf("s[0] is %c\n" , s[0]);
+    printf("s[0] is %c\n" , *s[0]);
     return (0) ;
 }
