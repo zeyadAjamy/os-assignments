c58b8cb6fd5ab6e9861df4bdccd697b2284530d0
zeyadAjamy <wezza2003@gmail.com>
2022-10-25 23:55:00 +0300

    edited solution for ex3

--- week07/ex4.c
+++ week07/ex4.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void * amazing_realloc(void * pointer, size_t new_size){
-    // If ptr is NULL, the call is equivalent to malloc(size)
+void * amazing_realloc(void * pointer, size_t old_size, size_t new_size){
+    // if ptr is NULL, the call is equivalent to malloc(size)
     if(pointer == NULL){ 
         return malloc(new_size);
     }
-    // If size is equal to zero, the call is equivalent to free(ptr)
+    // if size is equal to zero, the call is equivalent to free(ptr)
     if(new_size == 0){
         free(pointer);
         return NULL;
@@ -19,11 +20,63 @@ void * amazing_realloc(void * pointer, size_t new_size){
     }
     
     // Copy the data from the old memory to the new memory
-    for(int i = 0; i < new_size; i++){
-        *(char *)(new_ptr + i) = *(char *)(pointer + i);
-    }
-
+    size_t min_size = old_size < new_size ? old_size : new_size;
+    memcpy(new_ptr, pointer, min_size * sizeof(pointer));
+    
     // Deallocate the memory, so the TAs cannot find memory leaks, vol2:)
     free(pointer);
     return new_ptr;
+}
+
+
+int main()
+{
+
+    // Allows you to generate random number
+    srand(time(NULL));
+
+    // Allows user to specify the original array size, stored in variable n1.
+    printf("Enter original array size: ");
+    int n1 = 0;
+    scanf("%d", &n1);
+
+    // Create a new array of n1 ints
+    int *a1 = (int *) malloc(n1 * sizeof(int));
+    for (int i = 0; i < n1; i++)
+    {
+        // Set each value in a1 to 100
+        *(a1 + i) = 100;
+        // Print each element out (to make sure things look right)
+        printf("%d ", *(a1 + i));
+    }
+
+    // User specifies the new array size, stored in variable n2.
+    printf("\nEnter new array size: ");
+    int n2 = 0;
+    scanf("%d", &n2);
+
+    // Dynamically change the array to size n2
+    a1 = (int *) amazing_realloc(a1, n1, n2 * sizeof(int));
+
+    // If the new array is a larger size, set all new members to 0. Reason: dont want to use uninitialized variables.
+
+    if (n2 > n1)
+    {
+        for (int i = n1; i < n2; i++)
+        {
+            *(a1 + i) = 0;
+        }
+    }
+
+    for (int i = 0; i < n2; i++)
+    {
+        // Print each element out (to make sure things look right)
+        printf("%d ", *(a1 + i));
+    }
+    printf("\n");
+
+    // Done with array now, done with program :D
+    free(a1);
+
+    return 0;
 }
