74beb3e521454d8675b7564bb6d4a5f1ad0dede1
zeyadAjamy <wezza2003@gmail.com>
2022-10-25 23:55:00 +0300

    solved assignment 5

---
+++ week07/ex2.sh
@@ -0,0 +1,7 @@
+# compile and run the program
+gcc -o ex2 ex2.c
+
+./ex2
+
+# remove the binary file
+rm ex2
---
+++ week07/ex3.sh
@@ -0,0 +1,6 @@
+# compile and run the program
+gcc -o ex3 ex3.c
+
+./ex3
+# remove the binary file
+rm ex3
